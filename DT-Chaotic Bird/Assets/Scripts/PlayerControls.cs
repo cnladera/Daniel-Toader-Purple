683cee8ee3f52cf16d513da06f2c863be0490e2a
cnladera <108157970+cnladera@users.noreply.github.com>
2023-05-11 13:54:57 -0700

    --

--- DT-Chaotic Bird/Assets/Scripts/PlayerControls.cs
+++ DT-Chaotic Bird/Assets/Scripts/PlayerControls.cs
@@ -9,7 +9,7 @@ public class PlayerControls : MonoBehaviour
     [Header("Game Controller Object for controlling this game")]
     public GameController gameController;
     [Header("Default Velocity")]
-    public float velocity = 1;
+    public float velocity = 5;
     //Phyisics for the funny looking bird
     private Rigidbody2D rb;
     // Height of the bird on the Y axis
@@ -20,17 +20,32 @@ public class PlayerControls : MonoBehaviour
     void Start()
     {
         //Game Controller component
-        gameController = GetComponent<gameController>();
+        gameController = GetComponent<GameController>();
         //Speed for the game is at a playing state
         Time.timeScale = 1;
         rb = GetComponent<Rigidbody2D>();
         //Object Height equals the size of the height of the sprite
-        objectHeight = transform.GetComponent<SpriteRender>().bounds.size.y / 2;
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //If the LMB is clicked
+        if (Input.GetMouseButtonDown(0))
+        {
+            //The bird will float upwards on the Y axis and float back down on Y axis
+            rb.velocity = Vector2.up * velocity;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject.Find("GameController").GetComponent<GameController>().GameOver();
+        {
+            Time.timeScale = 0;
+        }
 
     }
+
 }
--- DT-Chaotic Bird/Assets/Scripts/Spawner.cs
+++ DT-Chaotic Bird/Assets/Scripts/Spawner.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    //Object that will be attached to the script for spawning object
+    [Header("Spikes Object for controlling the game")]
+    public GameObject spikes;
+    //Height position of the spikes
+    [Header("Default Height")]
+    public float height;
     // Start is called before the first frame update
     void Start()
     {
-
+        //Start function repeating every four seconds
+        InvokeRepeating("InstantiateObjects", 1f, 4f);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        transform.position = new Vector3(5, Random.Range(-height, height), 0);
+    }
+    void InstantiateObjects()
+    {
+        Instantiate(spikes, transform.position, transform.rotation);
     }
 }
